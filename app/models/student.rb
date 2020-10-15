f55df0ae694b639ff69040a83d009728963c816a
Hanna Davis <hanna.e.davis@gmail.com>
2020-10-15 12:09:57 -0600

    Change belongs_to association

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,5 @@
 class Student < ApplicationRecord
-  has_many :courses
-  validates_presence_of :name
-  validates_presence_of :age
-  validates_presence_of :house
+  validates_presence_of :name, :age, :house
+  has_many :student_courses
+  has_many :courses, through: :student_courses
 end
